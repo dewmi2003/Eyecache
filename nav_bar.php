7ed0d522d41c3c5cad68651506a4aa37992cbd2c
dewmiima23 <dewminiimasha719@gmail.com>
2025-09-16 10:17:58 +0530

    Add files via upload

---
+++ nav_bar.php
@@ -0,0 +1,26 @@
+
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg fixed-top">
+  <a class="navbar-brand fw-bold" href="#">EyeCache</a>
+  <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+    <span class="fas fa-bars"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item"><a href="index.html" class="nav-link"><i class="fas fa-home"></i> Home</a></li>
+      <li class="nav-item"><a href="shop_page.html" class="nav-link"><i class="fas fa-store"></i> Shop</a></li>
+      <li class="nav-item"><a href="OrderDashboard.php" class="nav-link"><i class="fas fa-shopping-cart"></i> Cart</a></li>
+      <li class="nav-item"><a href="User.php" class="nav-link"><i class="fas fa-user"></i> User</a></li>
+      <li class="nav-item"><a href="#about" class="nav-link"><i class="fas fa-info-circle"></i> About Us</a></li>
+      <li class="nav-item"><a href="#contact" class="nav-link"><i class="fas fa-envelope"></i> Contact</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+
+
+
+</body>
+</html>
